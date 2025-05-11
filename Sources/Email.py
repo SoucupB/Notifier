43801af33328dd82d726f0ee0db7d90d17a01ac9
SoucupB <bogdan.soucup@gmail.com>
2025-05-12 02:01:45 +0300

    WIP

---
+++ Sources/Email.py
@@ -0,0 +1,38 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class EmailDriver():
+  def __init__(self):
+    self.email = os.getenv("EMAIL_SENDER")
+    self.password = os.getenv("EMAIL_PASSWORD")
+    pass
+
+  def basicSend(self, to, subject, text):
+    msg = MIMEMultipart()
+    msg['From'] = self.email
+    msg['To'] = to
+    msg['Subject'] = subject
+    html_body = f"""
+    <html>
+    <body>
+      <div>{text}</div>
+    </body>
+    </html>
+    """
+    msg.attach(MIMEText(html_body, 'html'))
+    try:
+      with smtplib.SMTP('smtp.mail.yahoo.com', 587) as server:
+        # server.set_debuglevel(1)
+        server.starttls()
+        server.login(self.email, self.password) 
+        server.sendmail(self.email, to, msg.as_string())
+        print("HTML email sent successfully!")
+    except smtplib.SMTPException as e:
+      print(f"Error: {e}")
+    except Exception as e:
+      print(f"Unexpected Error: {e}")
--- Sources/Main.py
+++ Sources/Main.py
@@ -1,11 +1,14 @@
 from Driver import WebDriver
 from selenium.webdriver.common.by import By
 from BRDData import BRDDriver
+from Email import EmailDriver
 
-driver = WebDriver()
+# driver = WebDriver()
 def getFinancialData():
-  brdDriver = BRDDriver(driver)
-  print(brdDriver.data['EUR'])
+  # brdDriver = BRDDriver(driver)
+  # print(brdDriver.data['EUR'])
+  eml = EmailDriver()
+  eml.basicSend("soucup.bogdan@gmail.com", "Test subject", "Yolo data")
 
 getFinancialData()
 
