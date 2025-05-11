2acb53ca3f86fc1bc07454fe75fe7b04ca2e150d
SoucupB <bogdan.soucup@gmail.com>
2025-05-12 01:41:00 +0300

    Add financial data

--- Sources/BRDData.py
+++ Sources/BRDData.py
@@ -5,7 +5,35 @@ class BRDDriver():
   def __init__(self, driver):
     self.driver = driver
     self.driver.open("https://www.brd.ro/curs-valutar-si-dobanzi-de-referinta")
+    self.data = {}
     self._loadData()
+
+  def _loadCurrencies(self, parent):
+    self.currencyValues = []
+    index = 0
+    for curr in parent:
+      if not index:
+        index += 1
+        continue
+      val = curr.get_attribute('innerText')
+      self.data[val] = {}
+      self.currencyValues.append(val)
+      index += 1
+
+  def _loadValues(self, parent, key):
+    index = 0
+    for curr in parent:
+      if not index:
+        index += 1
+        continue
+      val = curr.get_attribute('innerText')
+      self.data[self.currencyValues[index - 1]][key] = val
+      index += 1
+  def _loadBuyingValue(self, parent):
+    self._loadValues(parent, 'buy_value')
+
+  def _loadSellingValue(self, parent):
+    self._loadValues(parent, 'sell_value')
   
   def _loadData(self):
     table = self.driver.searchElement(By.ID, 'tabAccountExchangeRates')
@@ -17,6 +45,11 @@ class BRDDriver():
     financialDivs = self.driver.children(childTables[0])
     if not len(financialDivs):
       return 
-    currencies = self.driver.children(financialDivs[1], 'p')
-    for a in currencies:
-      print(a.tag_name)
+    currenciesElements = self.driver.children(financialDivs[1])
+    self._loadCurrencies(currenciesElements)
+    buyingValue = self.driver.children(financialDivs[3])
+    self._loadBuyingValue(buyingValue)
+    sellingValue = self.driver.children(financialDivs[4])
+    self._loadSellingValue(sellingValue)
+
+    print(self.data)
--- Sources/Main.py
+++ Sources/Main.py
@@ -6,17 +6,6 @@ driver = WebDriver()
 def getFinancialData():
   brdDriver = BRDDriver(driver)
 
-  # table = driver.searchElement(By.ID, 'tabAccountExchangeRates')
-  # if not table:
-  #   return 
-  # childTables = driver.children(table)
-  # if not len(childTables):
-  #   return
-  
-  # financialDivs = driver.children(childTables[0])
-  # for a in financialDivs:
-  #   print(a.tag_name)
-
 getFinancialData()
 
 
