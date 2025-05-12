d159d2f9568ed5b632995382f729955be8559134
SoucupB <bogdan.soucup@gmail.com>
2025-05-12 03:01:57 +0300

    WIP

--- Sources/Main.py
+++ Sources/Main.py
@@ -5,14 +5,15 @@ from Email import EmailDriver
 import os
 from dotenv import load_dotenv
 load_dotenv()
-driver = WebDriver()
+# driver = WebDriver()
 def getFinancialData():
-  brdDriver = BRDDriver(driver)
-  response = ""
-  for key, value in brdDriver.data.items():
-    response += f"<p>Currency: {key}, Selling by: {value['sell_value']}, Buying by: {value['buy_value']}</p>"
-  eml = EmailDriver()
-  eml.basicSend(os.getenv("EMAIL_SEND_TO"), "Currency data", response)
+  # brdDriver = BRDDriver(driver)
+  # response = ""
+  # for key, value in brdDriver.data.items():
+  #   response += f"<p>Currency: {key}, Selling by: {value['sell_value']}, Buying by: {value['buy_value']}</p>"
+  print(os.getenv("EMAIL_SEND_TO"))
+  # eml = EmailDriver()
+  # eml.basicSend(os.getenv("EMAIL_SEND_TO"), "Currency data", response)
 
 getFinancialData()
 print("DONE!")
